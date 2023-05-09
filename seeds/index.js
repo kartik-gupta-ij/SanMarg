2746501667b869f418426a904bd4594fda97d142
Kartik Gupta <kartikgupta7267@gmail.com>
2023-05-10 01:08:35 +0530

    seeding

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -21,31 +26,30 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 300; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+    for (let i = 0; i < 200; i++) {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            author: '5f5c330c2cd79d538f2c66d9',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            author: '645a99faab28f11b24a0f579',
+            location: `${cities[i].city}, ${cities[i].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
             geometry: {
                 type: "Point",
                 coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude,
+                    cities[i].lon,
+                    cities[i].lat,
                 ]
             },
             images: [
                 {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                    url: 'https://res.cloudinary.com/dbuho0uxm/image/upload/v1683659547/YelpCamp/dbjub8xmhvmj66zm8nzs.jpg',
+                    filename: 'YelpCamp/dbjub8xmhvmj66zm8nzs'
                 },
                 {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                    url: 'https://res.cloudinary.com/dbuho0uxm/image/upload/v1683659518/YelpCamp/jvbxt5qp3nlx3ujypaxn.jpg',
+                    filename: 'YelpCamp/jvbxt5qp3nlx3ujypaxn'
                 }
             ]
         })
