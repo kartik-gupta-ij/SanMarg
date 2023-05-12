cf0c07a942011d02316f83a89c9a829fd33af8cb
Siddhartha-Mishra <76718773+SiddharthaMishra-dev@users.noreply.github.com>
2023-05-13 00:30:55 +0530

    Update

--- controllers/issues.js
+++ controllers/issues.js
@@ -18,9 +18,9 @@ async function main(campground) {
       host: "smtp.gmail.com",
       secure: false, // true for 465, false for other ports
       auth: {
-        user: "sid.mishra190601@gmail.com",
+        user: "",
         // user:`${process.env.NODEMAILER_EMAIL}`, // generated ethereal user
-        pass: "somviedqzrgdgyyh", // generated ethereal password
+        pass: "", // generated ethereal password
         // pass: `${process.env.NODEMAILER_KEY}`
       },
     });
